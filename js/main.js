e19724852c00bc93d3e14895af4c090cd5c05650
Nick Doyle <hello@nickdoyle.com>
2023-12-05 14:50:37 -0500

    Completed Exercise

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 // Start writing JavaScript here!
-alert("Hello World!")
+const button = document.querySelector('button')
+const body = document.body
+
+button.addEventListener('click', evt => {
+    // console.log("Push the screen")
+    // if (body.classList.contains('offsite-is-open')) {
+    //     body.classList.remove('offsite-is-open')
+    // } else {
+    //     body.classList.add('offsite-is-open')
+    // }
+    // body.classList.toggle('offsite-is-open')
+
+})
+
+const button2 = document.querySelector('.container button')
+const body2 = document.querySelector('body')
+
+button2.addEventListener('click', evt => {
+    console.log("Push the screen")
+    // body.classList.add('offsite-is-open')
+    // if (body2.classList.contains('offsite-is-open')) {
+    //     body2.classList.remove('offsite-is-open')
+    // } else {
+    //     body2.classList.add('offsite-is-open')
+    // }
+    body2.classList.toggle('offsite-is-open')
+})
