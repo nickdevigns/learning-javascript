2f8abb807b3faba7427c84447d4de2a71e62084c
Nick Doyle <hello@nickdoyle.com>
2023-12-06 17:25:49 -0500

    Completed Exercise

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 // Start writing JavaScript here!
-alert("Hello World!")
+const modalOpenButton = document.querySelector('.jsModalButton')
+modalOpenButton.addEventListener('click', evt => {
+    document.body.classList.add('modal-is-open')
+})
+
+const modalCloseButton = document.querySelector('.jsModalClose')
+modalCloseButton.addEventListener('click', evt => {
+    document.body.classList.remove('modal-is-open')
+})
