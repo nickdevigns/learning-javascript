483f55fde0816246541e0c099a7c55492e60656d
Nick Doyle <hello@nickdoyle.com>
2024-01-11 17:14:52 -0500

    Chapter 5 - Lesson 7: Completed Exercise - Added Lesson Comments

--- js/main.js
+++ js/main.js
@@ -6,18 +6,25 @@ const tabby = document.querySelector('.tabby')
 const tabs = Array.from(tabby.querySelectorAll('.tab'))
 const tabContents = Array.from(tabby.querySelectorAll('.tab-content'))
 
-// Add event listeners to each tab
-
+// 1. Add event listeners to each tab
 tabs.forEach(tab => {
   tab.addEventListener('click', e => {
+    // 2. Find the tab they clicked
     // console.log(tab)
+
+    // 3. Find the corresponding tab content
     const target = tab.dataset.theme
     // console.log(target)
     const tabContent = tabby.querySelector('#' + target)
     // console.log(tabContent)
+
+    // 4. Remove is-selected from other tabs to de-emphasize them
     tabs.forEach(t => t.classList.remove('is-selected'))
+    // 5. Add is-selected to the tab they clicked to emphasize it
     tab.classList.add('is-selected')
+    // 6. Remove is-selected from other tab content to hide them
     tabContents.forEach(c => c.classList.remove('is-selected'))
+    // 7. Add is-selected to the tab content to show it
     tabContent.classList.add('is-selected')
   })
 })
