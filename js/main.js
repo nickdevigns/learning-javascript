c3bb18e66733d3c19966c1347a02a6948fac4ff2
Nick Doyle <hello@nickdoyle.com>
2024-01-18 17:43:10 -0500

    Initial commit for Chapter 6 - Lesson 6: Closing the Modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 // Start writing JavaScript here!
-alert("Hello World!");
-// FIXME: Error
-// TODO: Work to do for yourself
+const modalButton = document.querySelector('.jsModalButton')
+const modalCloseButton = document.querySelector('.jsModalClose')
+
+modalButton.addEventListener('click', event => {
+  document.body.classList.add('modal-is-open')
+})
+
+modalCloseButton.addEventListener('click', event => {
+  document.body.classList.remove('modal-is-open')
+})
