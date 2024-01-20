f464538389ef929402533378d8e742313eed523f
Nick Doyle <hello@nickdoyle.com>
2024-01-20 14:37:25 -0500

    Chapter 6 - Lesson 6: Completed Exercise

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Start writing JavaScript here!
 const modalButton = document.querySelector('.jsModalButton')
 const modalCloseButton = document.querySelector('.jsModalClose')
+const modalOverlay = document.querySelector('.modal-overlay')
 
 modalButton.addEventListener('click', event => {
   document.body.classList.add('modal-is-open')
@@ -8,4 +9,14 @@ modalButton.addEventListener('click', event => {
 
 modalCloseButton.addEventListener('click', event => {
   document.body.classList.remove('modal-is-open')
+})
+
+modalOverlay.addEventListener('click', e => {
+  // document.body.classList.remove('modal-is-open')
+  console.log(e.target)
+
+  if (!e.target.closest('.modal')) {
+    // Close modal
+    document.body.classList.remove('modal-is-open')
+  }
 })
